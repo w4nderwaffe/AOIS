bba7fba591f435e0e0af6cbd2ce43a187fe03d48
w4nderwaffe <vanyusha_nikolaev_1997@bk.ru>
2026-03-21 09:52:38 +0300

    Refactor integer and float operation functions

--- LAB1/src/main.py
+++ LAB1/src/main.py
@@ -10,6 +10,14 @@ def read_int(prompt):
             print('Введите целое число.')
 
 
+def read_non_negative_int(prompt):
+    while True:
+        value = read_int(prompt)
+        if value >= 0:
+            return value
+        print('Введите неотрицательное целое число.')
+
+
 def read_float(prompt):
     while True:
         value = input(prompt).strip().replace(',', '.')
@@ -26,107 +34,166 @@ def print_integer_codes(value):
     print('Дополнительный код:', Formatter.bits(IntegerCodes.to_twos_complement(value)))
 
 
-def print_integer_operations(a, b):
-    print(f'\nОперации для целых чисел {a} и {b}:')
-
-    add_result = IntegerCodes.add(a, b)
-    print('Сложение (доп. код):')
-    print('  A      =', Formatter.bits(add_result['a_bits']))
-    print('  B      =', Formatter.bits(add_result['b_bits']))
-    print('  Result =', Formatter.bits(add_result['result_bits']))
-    print('  Decimal=', add_result['result_value'])
-
-    sub_result = IntegerCodes.subtract(a, b)
-    print('Вычитание (доп. код):')
-    print('  A      =', Formatter.bits(sub_result['a_bits']))
-    print('  B      =', Formatter.bits(sub_result['b_bits']))
-    print('  Result =', Formatter.bits(sub_result['result_bits']))
-    print('  Decimal=', sub_result['result_value'])
-
-    mul_result = DirectCodeArithmetic.multiply(a, b)
-    print('Умножение (прямой код):')
-    print('  A      =', Formatter.bits(mul_result['a_bits']))
-    print('  B      =', Formatter.bits(mul_result['b_bits']))
-    print('  Result =', Formatter.bits(mul_result['result_bits']))
-    print('  Decimal=', mul_result['result_value'])
-
+def run_integer_addition():
+    a = read_int('\nВведите первое целое число: ')
+    b = read_int('Введите второе целое число: ')
     try:
-        div_result = DirectCodeArithmetic.divide(a, b)
-        print('Деление (прямой код):')
-        print('  A      =', Formatter.bits(div_result['a_bits']))
-        print('  B      =', Formatter.bits(div_result['b_bits']))
-        print('  Result =', Formatter.bits(div_result['result_bits']))
-        print('  Decimal=', Formatter.decimal(div_result['result_value']))
-    except ZeroDivisionError:
-        print('Деление (прямой код): деление на ноль невозможно')
+        result = IntegerCodes.add(a, b)
+        print('\nСложение в дополнительном коде:')
+        print('A      =', Formatter.bits(result['a_bits']))
+        print('B      =', Formatter.bits(result['b_bits']))
+        print('Result =', Formatter.bits(result['result_bits']))
+        print('Decimal=', result['result_value'])
+    except OverflowError as error:
+        print('\nОшибка:', error)
 
 
-def print_float_operations(a, b):
-    print(f'\nIEEE-754 операции для чисел {a} и {b}:')
-
-    add_result = IEEE754Binary32.add(a, b)
-    print('IEEE-754 сложение:')
-    print('  A      =', Formatter.bits(add_result['a_bits']))
-    print('  B      =', Formatter.bits(add_result['b_bits']))
-    print('  Result =', Formatter.bits(add_result['result_bits']))
-    print('  Decimal=', Formatter.decimal(add_result['result_value']))
-
-    sub_result = IEEE754Binary32.subtract(a, b)
-    print('IEEE-754 вычитание:')
-    print('  A      =', Formatter.bits(sub_result['a_bits']))
-    print('  B      =', Formatter.bits(sub_result['b_bits']))
-    print('  Result =', Formatter.bits(sub_result['result_bits']))
-    print('  Decimal=', Formatter.decimal(sub_result['result_value']))
-
-    mul_result = IEEE754Binary32.multiply(a, b)
-    print('IEEE-754 умножение:')
-    print('  A      =', Formatter.bits(mul_result['a_bits']))
-    print('  B      =', Formatter.bits(mul_result['b_bits']))
-    print('  Result =', Formatter.bits(mul_result['result_bits']))
-    print('  Decimal=', Formatter.decimal(mul_result['result_value']))
-
+def run_integer_subtraction():
+    a = read_int('\nВведите уменьшаемое: ')
+    b = read_int('Введите вычитаемое: ')
     try:
-        div_result = IEEE754Binary32.divide(a, b)
-        print('IEEE-754 деление:')
-        print('  A      =', Formatter.bits(div_result['a_bits']))
-        print('  B      =', Formatter.bits(div_result['b_bits']))
-        print('  Result =', Formatter.bits(div_result['result_bits']))
-        print('  Decimal=', Formatter.decimal(div_result['result_value']))
-    except ZeroDivisionError:
-        print('IEEE-754 деление: деление на ноль невозможно')
+        result = IntegerCodes.subtract(a, b)
+        print('\nВычитание в дополнительном коде:')
+        print('A      =', Formatter.bits(result['a_bits']))
+        print('B      =', Formatter.bits(result['b_bits']))
+        print('Result =', Formatter.bits(result['result_bits']))
+        print('Decimal=', result['result_value'])
+    except OverflowError as error:
+        print('\nОшибка:', error)
 
 
-def print_bcd_operation(a, b):
-    print(f'\nBCD 2421 для чисел {a} и {b}:')
+def run_integer_multiplication():
+    a = read_int('\nВведите первое целое число: ')
+    b = read_int('Введите второе целое число: ')
     try:
-        result = BCD2421.add(a, b)
-        print('BCD 2421 сложение:')
-        print('  A      =', Formatter.bits(result['a_bits']))
-        print('  B      =', Formatter.bits(result['b_bits']))
-        print('  Result =', Formatter.bits(result['result_bits']))
-        print('  Decimal=', result['result_value'])
-    except ValueError as error:
-        print('BCD 2421 сложение:', error)
+        result = DirectCodeArithmetic.multiply(a, b)
+        print('\nУмножение в прямом коде:')
+        print('A      =', Formatter.bits(result['a_bits']))
+        print('B      =', Formatter.bits(result['b_bits']))
+        print('Result =', Formatter.bits(result['result_bits']))
+        print('Decimal=', result['result_value'])
+    except OverflowError as error:
+        print('\nОшибка:', error)
+
+
+def run_integer_division():
+    a = read_int('\nВведите делимое: ')
+    b = read_int('Введите делитель: ')
+    try:
+        result = DirectCodeArithmetic.divide(a, b)
+        print('\nДеление в прямом коде:')
+        print('A      =', Formatter.bits(result['a_bits']))
+        print('B      =', Formatter.bits(result['b_bits']))
+        print('Result =', Formatter.bits(result['result_bits']))
+        print('Decimal=', Formatter.decimal(result['result_value']))
+    except (OverflowError, ZeroDivisionError) as error:
+        print('\nОшибка:', error)
+
+
+def run_float_addition():
+    a = read_float('\nВведите первое вещественное число: ')
+    b = read_float('Введите второе вещественное число: ')
+    result = IEEE754Binary32.add(a, b)
+    print('\nIEEE-754 сложение:')
+    print('A      =', Formatter.bits(result['a_bits']))
+    print('B      =', Formatter.bits(result['b_bits']))
+    print('Result =', Formatter.bits(result['result_bits']))
+    print('Decimal=', Formatter.decimal(result['result_value']))
+
+
+def run_float_subtraction():
+    a = read_float('\nВведите уменьшаемое: ')
+    b = read_float('Введите вычитаемое: ')
+    result = IEEE754Binary32.subtract(a, b)
+    print('\nIEEE-754 вычитание:')
+    print('A      =', Formatter.bits(result['a_bits']))
+    print('B      =', Formatter.bits(result['b_bits']))
+    print('Result =', Formatter.bits(result['result_bits']))
+    print('Decimal=', Formatter.decimal(result['result_value']))
+
+
+def run_float_multiplication():
+    a = read_float('\nВведите первое вещественное число: ')
+    b = read_float('Введите второе вещественное число: ')
+    result = IEEE754Binary32.multiply(a, b)
+    print('\nIEEE-754 умножение:')
+    print('A      =', Formatter.bits(result['a_bits']))
+    print('B      =', Formatter.bits(result['b_bits']))
+    print('Result =', Formatter.bits(result['result_bits']))
+    print('Decimal=', Formatter.decimal(result['result_value']))
+
+
+def run_float_division():
+    a = read_float('\nВведите делимое: ')
+    b = read_float('Введите делитель: ')
+    try:
+        result = IEEE754Binary32.divide(a, b)
+        print('\nIEEE-754 деление:')
+        print('A      =', Formatter.bits(result['a_bits']))
+        print('B      =', Formatter.bits(result['b_bits']))
+        print('Result =', Formatter.bits(result['result_bits']))
+        print('Decimal=', Formatter.decimal(result['result_value']))
+    except ZeroDivisionError as error:
+        print('\nОшибка:', error)
+
+
+def run_bcd_addition():
+    a = read_non_negative_int('\nВведите первое неотрицательное число: ')
+    b = read_non_negative_int('Введите второе неотрицательное число: ')
+    result = BCD2421.add(a, b)
+    print('\nСложение в BCD 2421:')
+    print('A      =', Formatter.bits(result['a_bits']))
+    print('B      =', Formatter.bits(result['b_bits']))
+    print('Result =', Formatter.bits(result['result_bits']))
+    print('Decimal=', result['result_value'])
+
+
+def print_menu():
+    print('\nВыберите операцию:')
+    print('1. Показать прямой, обратный и дополнительный коды числа')
+    print('2. Сложение целых чисел в дополнительном коде')
+    print('3. Вычитание целых чисел в дополнительном коде')
+    print('4. Умножение целых чисел в прямом коде')
+    print('5. Деление целых чисел в прямом коде')
+    print('6. Сложение вещественных чисел IEEE-754')
+    print('7. Вычитание вещественных чисел IEEE-754')
+    print('8. Умножение вещественных чисел IEEE-754')
+    print('9. Деление вещественных чисел IEEE-754')
+    print('10. Сложение чисел в BCD 2421')
+    print('0. Выход')
 
 
 def main():
-    print('Лабораторная работа №1')
-    print('Введите данные для демонстрации операций.')
+    while True:
+        print_menu()
+        choice = input('\nВведите номер операции: ').strip()
 
-    integer_a = read_int('\nВведите первое целое число: ')
-    integer_b = read_int('Введите второе целое число: ')
-
-    float_a = read_float('\nВведите первое вещественное число: ')
-    float_b = read_float('Введите второе вещественное число: ')
-
-    bcd_a = read_int('\nВведите первое неотрицательное число для BCD 2421: ')
-    bcd_b = read_int('Введите второе неотрицательное число для BCD 2421: ')
-
-    print_integer_codes(integer_a)
-    print_integer_codes(integer_b)
-    print_integer_operations(integer_a, integer_b)
-    print_float_operations(float_a, float_b)
-    print_bcd_operation(bcd_a, bcd_b)
+        if choice == '1':
+            value = read_int('\nВведите целое число: ')
+            print_integer_codes(value)
+        elif choice == '2':
+            run_integer_addition()
+        elif choice == '3':
+            run_integer_subtraction()
+        elif choice == '4':
+            run_integer_multiplication()
+        elif choice == '5':
+            run_integer_division()
+        elif choice == '6':
+            run_float_addition()
+        elif choice == '7':
+            run_float_subtraction()
+        elif choice == '8':
+            run_float_multiplication()
+        elif choice == '9':
+            run_float_division()
+        elif choice == '10':
+            run_bcd_addition()
+        elif choice == '0':
+            print('\nЗавершение программы.')
+            break
+        else:
+            print('\nНекорректный пункт меню.')
 
 
 if __name__ == '__main__':
